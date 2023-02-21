5322387fe015834b35b4775936af76aa277d8eb8
Guilherme Silva Oliveira <gsoliveira13>
2023-02-21 10:18:24 -0300

    build: created folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from './auth/auth.module';
+import { BetsModule } from './bets/bets.module';
+import { GamesModule } from './games/games.module';
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [],
+  imports: [UsersModule, GamesModule, BetsModule, AuthModule],
   controllers: [],
   providers: [],
 })
