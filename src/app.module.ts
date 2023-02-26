98a519cdcef5e26e0408d630704d75ac20d2df03
Guilherme Silva Oliveira <gsoliveira13>
2023-02-26 17:58:10 -0300

    feat: beginning the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-import { UsersModule } from './app/users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: process.env.TYPEORM_CONNECTION,
-      host: process.env.TYPEORM_HOST,
-      port: process.env.TYPEORM_PORT,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      entities: [__dirname + '/**/*.entity{.js,.ts}'],
-      synchronize: true,
-    } as TypeOrmModuleOptions),
-    UsersModule,
-    AuthModule,
-  ],
+  imports: [UsersModule],
   controllers: [],
   providers: [],
 })
--- src/app/users/dto/update-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class UpdateUserDto {
-  @IsNotEmpty()
-  firstName: string;
-
-  @IsNotEmpty()
-  lastName: string;
-}
