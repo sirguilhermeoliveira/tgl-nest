6092784abcb423e59bc6d2dffb4411477bde6ee4
Guilherme Silva Oliveira <gsoliveira13>
2023-04-25 23:26:31 -0300

    chore(admin): admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, AdminModule],
   controllers: [AppController],
   providers: [
     AppService,
