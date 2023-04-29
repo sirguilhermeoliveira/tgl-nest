5fddfc780f40ae8079861c5864bfae4281c840bf
Guilherme Silva Oliveira <gsoliveira13>
2023-04-29 03:02:55 -0300

    remove: hello world endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,18 +3,11 @@ import { ApiTags } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 import { CurrentUser } from './auth/decorators/current-user.decorator';
-import { IsPublic } from './auth/decorators/is-public.decorator';
 
 @Controller()
 @ApiTags()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @IsPublic()
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 
   @Get('me')
   getMe(@CurrentUser() user): string {
