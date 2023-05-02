f7138dc50ba6f5c355536bae269191dcdee7a5ea
Guilherme Silva Oliveira <gsoliveira13>
2023-05-02 16:14:23 -0300

    feat: handleDailyTask

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -10,14 +10,25 @@ export class TasksService {
   ) {}
   async handleDailyTask() {
     const users = await this.prisma.user.findMany();
-    return users.map((user) =>
-      this.mailerService.sendMail({
-        to: user.email,
-        from: 'noreply@nestjs.com',
-        subject: 'Welcome to TGL Nest!',
-        template: 'taskBets',
-        context: { name: user.name },
-      }),
-    );
+
+    users.forEach((user) => {
+      this.prisma.bet.findMany({ where: { user_id: user.id } }).then((bets) => {
+        const currentDate = new Date();
+        const lastBetDate = bets[bets.length - 1];
+        const targetDate = lastBetDate.created_At;
+        const oneWeekLater = new Date(targetDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+        if (currentDate > oneWeekLater) {
+          this.mailerService.sendMail({
+            to: user.email,
+            from: 'noreply@nestjs.com',
+            subject: 'Come back to TGL Nest!',
+            template: 'taskBets',
+            context: {
+              name: user.name,
+            },
+          });
+        }
+      });
+    });
   }
 }
