2200deb22eb124f30ad64af2822f73b6ce4ca7ce
Guilherme Silva Oliveira <gsoliveira13>
2023-05-02 15:11:05 -0300

    fix: prisma module now on tasks module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
