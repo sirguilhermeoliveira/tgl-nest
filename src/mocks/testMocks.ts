c7e65c144671287ead45a6ea6d410af0ccea16d5
Guilherme Silva Oliveira <gsoliveira13>
2023-05-03 22:00:38 -0300

    feat: game mock

--- src/mocks/testMocks.ts
+++ src/mocks/testMocks.ts
@@ -1,4 +1,4 @@
-export const user_1 = {
+export const userMock_1 = {
   id: 1,
   email: 'user1@example.com',
   name: 'User 1',
@@ -8,7 +8,7 @@ export const user_1 = {
   password: 'Abc123',
 };
 
-export const user_2 = {
+export const userMock_2 = {
   id: 2,
   email: 'user2@example.com',
   name: 'User 2',
@@ -18,7 +18,7 @@ export const user_2 = {
   password: 'Abc123',
 };
 
-export const bet_1 = {
+export const betMock_1 = {
   id: 1,
   user_id: 1,
   game_id: 1,
@@ -26,10 +26,30 @@ export const bet_1 = {
   created_At: new Date(),
 };
 
-export const bet_2 = {
+export const betMock_2 = {
   id: 2,
   user_id: 2,
   game_id: 2,
   bet_numbers: '1,2,3,4,5,6,7,8,9,10',
   created_At: new Date(),
+};
+
+export const gameMock_1 = {
+  id: 1,
+  title: 'Test Title',
+  description: 'Test Description',
+  color: 'Test Color',
+  price: 3.2,
+  max_bet_numbers: 10,
+  bet_range: 60,
+};
+
+export const gameMock_2 = {
+  id: 2,
+  title: 'Test Title',
+  description: 'Test Description',
+  color: 'Test Color',
+  price: 4,
+  max_bet_numbers: 30,
+  bet_range: 100,
 };
