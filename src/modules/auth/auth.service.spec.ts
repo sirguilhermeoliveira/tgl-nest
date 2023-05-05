270d32b2bee250ec00ab9a537eb18726ebf07c6e
Guilherme Silva Oliveira <gsoliveira13>
2023-05-05 15:09:23 -0300

    chore: auth service test begin

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,19 +1,63 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { MailerService } from '@nestjs-modules/mailer';
 
+import { UserService } from '../../modules/users/user.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UserService,
+        PrismaService,
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(() => 'mockToken'),
+          },
+        },
+        {
+          provide: MailerService,
+          useValue: {
+            sendMail: jest.fn().mockResolvedValue(null),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('login', () => {
+    it('should return a user token', () => {
+      const user = {
+        id: 1,
+        email: 'test@test.com',
+        name: 'test',
+        password: 'password',
+      };
+      const loginRequestBody = {
+        email: user.email,
+        password: user.password,
+      };
+      const expectedToken = {
+        access_token: 'mockToken',
+      };
+      const jwtSignSpy = jest.spyOn(jwtService, 'sign');
+
+      expect(authService.login(loginRequestBody)).toEqual(expectedToken);
+      expect(jwtSignSpy).toHaveBeenCalledWith({
+        sub: user.id,
+        email: user.email,
+        name: user.name,
+      });
+    });
   });
 });
