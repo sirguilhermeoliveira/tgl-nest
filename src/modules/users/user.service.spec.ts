fb766dfe23db6ddf316745872d4b8670edbef186
Guilherme Silva Oliveira <gsoliveira13>
2023-05-05 23:20:21 -0300

    feat: create a user test

--- src/modules/users/user.service.spec.ts
+++ src/modules/users/user.service.spec.ts
@@ -1,19 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MailerService } from '@nestjs-modules/mailer';
 
+import { userMock_1 } from '../../mocks/users.mocks';
+import { PrismaService } from '../prisma/prisma.service';
 import { UserService } from './user.service';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('UsersService', () => {
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        PrismaService,
+        {
+          provide: UserService,
+          useValue: {
+            create: jest.fn().mockResolvedValue({ message: 'User created successfully!' }),
+          },
+        },
+        {
+          provide: MailerService,
+          useValue: {
+            sendMail: jest.fn().mockResolvedValue(null),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('Create', () => {
+    it('should create a user', async () => {
+      jest.spyOn(userService, 'create');
+      const result = await userService.create(userMock_1);
+      expect(result).toEqual({ message: 'User created successfully!' });
+    });
   });
 });
