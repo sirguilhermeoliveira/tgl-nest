c3ca88c2a4b753e4922c97047b0d4cab18791741
Guilherme Silva Oliveira <gsoliveira13>
2023-05-05 23:20:44 -0300

    feat: games service test

--- src/modules/games/games.service.spec.ts
+++ src/modules/games/games.service.spec.ts
@@ -1,19 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MailerService } from '@nestjs-modules/mailer';
 
+import { gameMock_1 } from '../../mocks/games.mocks';
+import { PrismaService } from '../prisma/prisma.service';
 import { GamesService } from './games.service';
 
 describe('GamesService', () => {
-  let service: GamesService;
+  let gamesService: GamesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GamesService],
+      providers: [
+        PrismaService,
+        {
+          provide: GamesService,
+          useValue: {
+            create: jest.fn().mockResolvedValue({ message: 'Game created successfully!' }),
+          },
+        },
+        {
+          provide: MailerService,
+          useValue: {
+            sendMail: jest.fn().mockResolvedValue(null),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<GamesService>(GamesService);
+    gamesService = module.get<GamesService>(GamesService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('Create', () => {
+    it('should create a game', async () => {
+      jest.spyOn(gamesService, 'create');
+      const result = await gamesService.create(gameMock_1);
+      expect(result).toEqual({ message: 'Game created successfully!' });
+    });
   });
 });
